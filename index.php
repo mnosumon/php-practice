c56bccb0b948fb1089385a5413ed7da555d502e3
mnosumon <mdnuruddin378350@gmail.com>
2024-12-29 20:43:50 +0600

    Create a PHP program to check if a string contains a specific substring.

--- index.php
+++ index.php
@@ -695,10 +695,10 @@ echo "<br>";
 
     echo "<br>";
 
+    // Create a PHP program to check if a string contains a specific substring.
 
     $string = "Amar sonar bangla ami tomay vlao";  // Original string
     $substring = "ami s";  // Substring to check for
-    
     // Check if the substring exists in the string
     if (strpos($string, $substring) !== false) {
         echo "this is striong";
